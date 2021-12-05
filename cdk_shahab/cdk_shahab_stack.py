79ceae1cbb887e0842966aa7c637e189086704a2
EC2 Default User <ec2-user@ip-10-1-3-181.us-east-2.compute.internal>
2021-12-05 22:00:48 +0000

    Committer: EC2 Default User <ec2-user@ip-10-1-3-181.us-east-2.compute.internal>  On branch main

--- cdk_shahab/cdk_shahab_stack.py
+++ cdk_shahab/cdk_shahab_stack.py
@@ -1,8 +1,9 @@
 from aws_cdk import (
-    aws_iam as iam,
-    aws_sqs as sqs,
-    aws_sns as sns,
-    aws_sns_subscriptions as subs,
+    aws_dynamodb as _dynamodb,
+    aws_iam as _iam,
+    aws_s3 as _s3,
+    aws_s3_notifications as _s3_notify,
+    aws_lambda as _lambda,
     core
 )
 
@@ -11,14 +12,23 @@ class CdkShahabStack(core.Stack):
 
     def __init__(self, scope: core.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
+        
+        my_table  = _dynamodb.Table( self, id = 'dynamoTable', table_name='employeetable'
+                                    ,partition_key= _dynamodb.Attribute(name='empid', type= _dynamodb.AttributeType.NUMBER ))
+                                    
+        my_bucket = _s3.Bucket(self, 's3bucket', bucket_name='employeebucket-assignment')
 
-        queue = sqs.Queue(
-            self, "CdkShahabQueue",
-            visibility_timeout=core.Duration.seconds(300),
-        )
-
-        topic = sns.Topic(
-            self, "CdkShahabTopic"
-        )
-
-        topic.add_subscription(subs.SqsSubscription(queue))
+        lambda_func = _lambda.Function( self, 'shahabassignmentfunction', runtime=_lambda.Runtime.PYTHON_3_7, 
+                                        handler= 'lambdalistener.handler', code = _lambda.Code.asset('lambdacode'),
+                                        environment={'BUCKET_NAME':
+                                                        my_bucket.bucket_name}
+                                      ) 
+       # Create trigger for Lambda function using suffix
+        notification = _s3_notify.LambdaDestination(lambda_func)
+        notification.bind(self, my_bucket)
+        
+        # Add Create Event only for .csv files
+        my_bucket.add_object_created_notification(
+           notification, _s3.NotificationKeyFilter(suffix='.csv'))
+           
+           
